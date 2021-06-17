cd3f56cb3b589ecd259869e90a40c42421d83922
DATBOI <alexsimons9999@gmail.com>
2021-06-17 17:57:55 -0500

    All you braces best get to steppin

--- doki-theme-visualstudio/ViewportAdornment1.cs
+++ doki-theme-visualstudio/ViewportAdornment1.cs
@@ -1,62 +1,57 @@
-﻿using Microsoft.VisualStudio.Text.Editor;
-using System;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Editor;
 
-namespace doki_theme_visualstudio
-{
+namespace doki_theme_visualstudio {
   /// <summary>
-  /// Adornment class that draws a square box in the top right hand corner of the viewport
+  ///   Adornment class that draws a square box in the top right hand corner of the viewport
   /// </summary>
-  internal sealed class ViewportAdornment1
-  {
+  internal sealed class ViewportAdornment1 {
     /// <summary>
-    /// The width of the square box.
+    ///   The width of the square box.
     /// </summary>
     private const double AdornmentWidth = 30;
 
     /// <summary>
-    /// The height of the square box.
+    ///   The height of the square box.
     /// </summary>
     private const double AdornmentHeight = 30;
 
     /// <summary>
-    /// Distance from the viewport top to the top of the square box.
+    ///   Distance from the viewport top to the top of the square box.
     /// </summary>
     private const double TopMargin = 30;
 
     /// <summary>
-    /// Distance from the viewport right to the right end of the square box.
+    ///   Distance from the viewport right to the right end of the square box.
     /// </summary>
     private const double RightMargin = 30;
 
     /// <summary>
-    /// Text view to add the adornment on.
-    /// </summary>
-    private readonly IWpfTextView view;
-
-    /// <summary>
-    /// Adornment image
-    /// </summary>
-    private readonly Image image;
-
-    /// <summary>
-    /// The layer for the adornment.
+    ///   The layer for the adornment.
     /// </summary>
     private readonly IAdornmentLayer adornmentLayer;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ViewportAdornment1"/> class.
-    /// Creates a square image and attaches an event handler to the layout changed event that
-    /// adds the the square in the upper right-hand corner of the TextView via the adornment layer
+    ///   Adornment image
     /// </summary>
-    /// <param name="view">The <see cref="IWpfTextView"/> upon which the adornment will be drawn</param>
-    public ViewportAdornment1(IWpfTextView view)
-    {
-      if (view == null)
-      {
-        throw new ArgumentNullException("view");
-      }
+    private readonly Image image;
+
+    /// <summary>
+    ///   Text view to add the adornment on.
+    /// </summary>
+    private readonly IWpfTextView view;
+
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="ViewportAdornment1" /> class.
+    ///   Creates a square image and attaches an event handler to the layout changed event that
+    ///   adds the the square in the upper right-hand corner of the TextView via the adornment layer
+    /// </summary>
+    /// <param name="view">The <see cref="IWpfTextView" /> upon which the adornment will be drawn</param>
+    public ViewportAdornment1(IWpfTextView view) {
+      if (view == null) throw new ArgumentNullException("view");
 
       this.view = view;
 
@@ -68,7 +63,7 @@ namespace doki_theme_visualstudio
       pen.Freeze();
 
       // Draw a square with the created brush and pen
-      System.Windows.Rect r = new System.Windows.Rect(0, 0, AdornmentWidth, AdornmentHeight);
+      var r = new Rect(0, 0, AdornmentWidth, AdornmentHeight);
       var geometry = new RectangleGeometry(r);
 
       var drawing = new GeometryDrawing(brush, pen, geometry);
@@ -77,32 +72,30 @@ namespace doki_theme_visualstudio
       var drawingImage = new DrawingImage(drawing);
       drawingImage.Freeze();
 
-      this.image = new Image
-      {
-        Source = drawingImage,
+      image = new Image {
+        Source = drawingImage
       };
 
-      this.adornmentLayer = view.GetAdornmentLayer("ViewportAdornment1");
+      adornmentLayer = view.GetAdornmentLayer("ViewportAdornment1");
 
-      this.view.LayoutChanged += this.OnSizeChanged;
+      this.view.LayoutChanged += OnSizeChanged;
     }
 
     /// <summary>
-    /// Event handler for viewport layout changed event. Adds adornment at the top right corner of the viewport.
+    ///   Event handler for viewport layout changed event. Adds adornment at the top right corner of the viewport.
     /// </summary>
     /// <param name="sender">Event sender</param>
     /// <param name="e">Event arguments</param>
-    private void OnSizeChanged(object sender, EventArgs e)
-    {
+    private void OnSizeChanged(object sender, EventArgs e) {
       // Clear the adornment layer of previous adornments
-      this.adornmentLayer.RemoveAllAdornments();
+      adornmentLayer.RemoveAllAdornments();
 
       // Place the image in the top right hand corner of the Viewport
-      Canvas.SetLeft(this.image, this.view.ViewportRight - RightMargin - AdornmentWidth);
-      Canvas.SetTop(this.image, this.view.ViewportTop + TopMargin);
+      Canvas.SetLeft(image, view.ViewportRight - RightMargin - AdornmentWidth);
+      Canvas.SetTop(image, view.ViewportTop + TopMargin);
 
       // Add the image to the adornment layer and make it relative to the viewport
-      this.adornmentLayer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, this.image, null);
+      adornmentLayer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, image, null);
     }
   }
 }
--- doki-theme-visualstudio/ViewportAdornment1TextViewCreationListener.cs
+++ doki-theme-visualstudio/ViewportAdornment1TextViewCreationListener.cs
@@ -1,24 +1,23 @@
-﻿using Microsoft.VisualStudio.Text.Editor;
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
-using System.ComponentModel.Composition;
 
-namespace doki_theme_visualstudio
-{
+namespace doki_theme_visualstudio {
   /// <summary>
-  /// Establishes an <see cref="IAdornmentLayer"/> to place the adornment on and exports the <see cref="IWpfTextViewCreationListener"/>
-  /// that instantiates the adornment on the event of a <see cref="IWpfTextView"/>'s creation
+  ///   Establishes an <see cref="IAdornmentLayer" /> to place the adornment on and exports the
+  ///   <see cref="IWpfTextViewCreationListener" />
+  ///   that instantiates the adornment on the event of a <see cref="IWpfTextView" />'s creation
   /// </summary>
   [Export(typeof(IWpfTextViewCreationListener))]
   [ContentType("text")]
   [TextViewRole(PredefinedTextViewRoles.Document)]
-  internal sealed class ViewportAdornment1TextViewCreationListener : IWpfTextViewCreationListener
-  {
+  internal sealed class ViewportAdornment1TextViewCreationListener : IWpfTextViewCreationListener {
     // Disable "Field is never assigned to..." and "Field is never used" compiler's warnings. Justification: the field is used by MEF.
 #pragma warning disable 649, 169
 
     /// <summary>
-    /// Defines the adornment layer for the scarlet adornment. This layer is ordered
-    /// after the selection layer in the Z-order
+    ///   Defines the adornment layer for the scarlet adornment. This layer is ordered
+    ///   after the selection layer in the Z-order
     /// </summary>
     [Export(typeof(AdornmentLayerDefinition))]
     [Name("ViewportAdornment1")]
@@ -28,11 +27,10 @@ namespace doki_theme_visualstudio
 #pragma warning restore 649, 169
 
     /// <summary>
-    /// Instantiates a ViewportAdornment1 manager when a textView is created.
+    ///   Instantiates a ViewportAdornment1 manager when a textView is created.
     /// </summary>
-    /// <param name="textView">The <see cref="IWpfTextView"/> upon which the adornment should be placed</param>
-    public void TextViewCreated(IWpfTextView textView)
-    {
+    /// <param name="textView">The <see cref="IWpfTextView" /> upon which the adornment should be placed</param>
+    public void TextViewCreated(IWpfTextView textView) {
       // The adorment will get wired to the text view events
       new ViewportAdornment1(textView);
     }
