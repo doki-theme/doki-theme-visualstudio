41f6e1a782607211c99287ed86a37b81b6fe04e7
DATBOI <alexsimons9999@gmail.com>
2021-06-20 10:28:35 -0500

    Able to create global asset directory

--- doki-theme-visualstudio/ThemeManager.cs
+++ doki-theme-visualstudio/ThemeManager.cs
@@ -5,19 +5,24 @@ using Task = System.Threading.Tasks.Task;
 
 namespace doki_theme_visualstudio {
   public class ThemeManager {
-
     public static async Task InitializePluginAsync(Package package) {
       ThreadHelper.ThrowIfOnUIThread();
 
       var assetDirectory = GetAssetDirectory(package);
-      // check if exists
-      await Console.Out.WriteLineAsync("Ayy lmao: " + assetDirectory);
     }
 
     private static string GetAssetDirectory(Package package) {
       var userLocalDataPath = package.UserLocalDataPath;
-      return userLocalDataPath.Substring(0, userLocalDataPath.LastIndexOf(Path.DirectorySeparatorChar)) +
-             Path.DirectorySeparatorChar + "dokiThemeAssets";
+      var assetsDirectory =
+        userLocalDataPath.Substring(
+          0,
+          userLocalDataPath.LastIndexOf(Path.DirectorySeparatorChar)
+        ) + Path.DirectorySeparatorChar + "dokiThemeAssets";
+      if (!Directory.Exists(assetsDirectory)) {
+        Directory.CreateDirectory(assetsDirectory);
+      }
+
+      return assetsDirectory;
     }
   }
 }
