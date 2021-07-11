d6fd3ff409fc22cdd18f54911061f349071e8779
DATBOI <alexsimons9999@gmail.com>
2021-07-11 05:50:55 -0500

    Less downloading these assets at startup

--- doki-theme-visualstudio/TheDokiTheme.cs
+++ doki-theme-visualstudio/TheDokiTheme.cs
@@ -12,9 +12,6 @@ namespace doki_theme_visualstudio {
       
       // depends on local storage service
       LocalAssetService.Init(package);
-      
-      await AssetManager.ResolveAssetUrlAsync(AssetCategory.Backgrounds, "zero_two_dark.png");
-      await AssetManager.ResolveAssetUrlAsync(AssetCategory.Stickers, "/yuruCamp/rin/dark/shima_rin_dark.png");
     }
   }
 }
