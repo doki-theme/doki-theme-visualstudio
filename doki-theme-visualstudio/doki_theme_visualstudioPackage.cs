4899b4673aa9e2e11c088b01dc65f66dfd65c105
DATBOI <alexsimons9999@gmail.com>
2021-07-15 17:15:41 -0500

    Removed unused command.

--- doki-theme-visualstudio/doki_theme_visualstudioPackage.cs
+++ doki-theme-visualstudio/doki_theme_visualstudioPackage.cs
@@ -60,8 +60,6 @@ namespace doki_theme_visualstudio {
         // When initialized asynchronously, the current thread may be a background thread at this point.
         // Do any initialization that requires the UI thread after switching to the UI thread.
         await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-        
-        await Command1.InitializeAsync(this);
     }
 
     #endregion
