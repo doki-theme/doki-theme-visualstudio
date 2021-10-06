33b3bad9f801536ad312b95702160fcdeb3be78f
DATBOI <alexsimons9999@gmail.com>
2021-10-06 05:55:24 -0500

    Solution builds

--- buildSrc/buildThemes.bat
+++ buildSrc/buildThemes.bat
@@ -2,9 +2,9 @@
 @REM yarn buildThemes 8c99ec4b-fda0-4ab7-95ad-a6bf80c3924b
 
 @REM nagatoro
-@REM yarn buildThemes ca45f54a-57cb-4d8c-82a6-d4b31c850bd6
+yarn buildThemes ca45f54a-57cb-4d8c-82a6-d4b31c850bd6
 
 @REM azuki
 @REM yarn buildThemes 83e94920-9ae2-4838-9b85-4c3fa22b67b8
 
-yarn buildThemes
+@REM yarn buildThemes
