06e76268a35ed61480e77da8a144b8b71c50f4f2
DATBOI <alexsimons9999@gmail.com>
2021-10-07 05:31:26 -0500

    Added all themes back

--- buildSrc/buildThemes.bat
+++ buildSrc/buildThemes.bat
@@ -1,5 +1,5 @@
 @REM Zero Two
-yarn buildThemes 8c99ec4b-fda0-4ab7-95ad-a6bf80c3924b
+@REM yarn buildThemes 8c99ec4b-fda0-4ab7-95ad-a6bf80c3924b
 
 @REM nagatoro
 @REM yarn buildThemes ca45f54a-57cb-4d8c-82a6-d4b31c850bd6
@@ -7,4 +7,4 @@ yarn buildThemes 8c99ec4b-fda0-4ab7-95ad-a6bf80c3924b
 @REM azuki
 @REM yarn buildThemes 83e94920-9ae2-4838-9b85-4c3fa22b67b8
 
-@REM yarn buildThemes
+yarn buildThemes
